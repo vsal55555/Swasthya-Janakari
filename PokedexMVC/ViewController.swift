9c14dabe1682fd5a7558e3767c7c30106964bb91
vsal55555 <vsal55555@gmail.com>
2020-08-25 13:55:43 +0545

    Initial Commit

---
+++ PokedexMVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokedexMVC
+//
+//  Created by BSAL-MAC on 8/25/20.
+//  Copyright © 2020 BSAL-MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
